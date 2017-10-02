841e09c9e20b4bdfd13261f8a8bdbbacf3794a82
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2017-10-02 19:55:54 +0900

    raw table builder added

---
+++ table/raw_table_builder.cc
@@ -0,0 +1,77 @@
+#include "raw_table_builder.h"
+#include "raw_block_builder.h"
+#include "db/global_index.h"
+
+namespace leveldb {
+
+struct RawTableBuilder::Rep {
+  Options options;
+  WritableFile* file;
+  Status status;
+  RawBlockBuilder data_block;
+  std::string last_key;
+  uint64_t num_entries;
+  bool closed;
+  GlobalIndex* global_index;
+
+  Rep(const Options& opt, WritableFile* f)
+      : options(opt),
+        file(f),
+        num_entries(0),
+        closed(false),
+        global_index(opt.global_index),
+        data_block(&options) {}
+};
+
+RawTableBuilder::RawTableBuilder(const Options& options, WritableFile* file)
+    : rep_(new Rep(options, file)) { }
+
+RawTableBuilder::~RawTableBuilder() {
+  delete rep_;
+}
+
+void RawTableBuilder::Add(const Slice &key, const Slice &value) {
+  Rep* r = rep_;
+  GlobalIndex* index = r->global_index;
+  assert(!r->closed);
+  if (!ok()) return;
+  r->last_key.assign(key.data(), key.size());
+  r->num_entries++;
+  r->data_block.Add(key, value);
+  uint64_t offset = r->data_block.GetBufferSize() - value.size() - 1;
+  index->Add(key.ToString(), offset, value.size(), false);
+}
+
+void RawTableBuilder::Flush() {
+  Rep* r = rep_;
+  assert(!r->closed);
+  if (!ok()) return;
+  if (r->data_block.empty()) return;
+  Slice raw = r->data_block.Finish();
+  r->data_block.Reset();
+  r->file->Append(raw);
+}
+
+Status RawTableBuilder::status() const {
+  return rep_->status;
+}
+
+Status RawTableBuilder::Finish() {
+  Rep *r = rep_;
+  Flush();
+  assert(!r->closed);
+  r->closed = true;
+  return r->status;
+}
+
+void RawTableBuilder::Abandon() {
+  Rep *r = rep_;
+  assert(!r->closed);
+  r->closed = true;
+}
+
+uint64_t RawTableBuilder::FileSize() {
+  return rep_->data_block.GetBufferSize();
+}
+
+} // namespace leveldb
---
+++ table/raw_table_builder.h
@@ -0,0 +1,38 @@
+#ifndef STORAGE_LEVELDB_DB_RAW_TABLE_BUILDER_H_
+#define STORAGE_LEVELDB_DB_RAW_TABLE_BUILDER_H_
+
+#include <leveldb/env.h>
+#include <leveldb/options.h>
+
+namespace leveldb {
+
+class RawTableBuilder {
+ public:
+  RawTableBuilder(const Options& options, WritableFile* file);
+
+  ~RawTableBuilder();
+
+  void Add(const Slice& key, const Slice& value);
+
+  void Flush();
+
+  Status status() const;
+
+  Status Finish();
+
+  void Abandon();
+
+  uint64_t FileSize();
+
+ private:
+  bool ok() const { return status().ok(); }
+
+  struct Rep;
+  Rep* rep_;
+
+};
+
+} // namespace leveldb
+
+
+#endif // STORAGE_LEVELDB_DB_RAW_TABLE_BUILDER_H
