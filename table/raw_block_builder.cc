2703a26aecd4f769c3486992d023d8e7a478ba1e
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2017-10-02 18:06:39 +0900

    raw block builder init

---
+++ table/raw_block_builder.cc
@@ -0,0 +1,36 @@
+#include "raw_block_builder.h"
+
+namespace leveldb {
+
+RawBlockBuilder::RawBlockBuilder(const leveldb::Options *options)
+    : options_(options),
+      counter_(0),
+      finished_(false) {
+
+}
+
+void RawBlockBuilder::Reset() {
+  buffer_.clear();
+  finished_ = false;
+  counter_ = 0;
+}
+
+void RawBlockBuilder::Add(const Slice &key, const Slice &value) {
+  buffer_.append(key.data(), key.size());
+  buffer_.append(value.data(), value.size());
+}
+
+Slice RawBlockBuilder::Finish() {
+  return Slice();
+}
+
+size_t RawBlockBuilder::CurrentSizeEstimate() const {
+  return buffer_.size() + sizeof(int);
+}
+
+uint64_t RawBlockBuilder::GetBufferSize() {
+  return buffer_.size();
+}
+
+
+} // namespace leveldb
