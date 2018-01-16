6670d35d7d18241a277052e8d54c535325ea9f6a
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2018-01-16 11:00:09 +0900

    compact function added to return memtable to be merged and accessed while merging

--- db/persistent_memtable.cc
+++ db/persistent_memtable.cc
@@ -1,33 +1,73 @@
 #include "persistent_memtable.h"
 
 namespace leveldb {
-PersistentMemtable::PersistentMemtable(const Comparator& cmp)
-    : cmp_(cmp), table_(cmp) {
 
+PersistentMemtable::PersistentMemtable(const Comparator* cmp)
+    : cmp_(cmp),
+      table_(new PersistentSkiplist(cmp)),
+      compaction_current_size(0),
+      mutex(new port::Mutex) { }
+
+PersistentMemtable::PersistentMemtable(const Comparator* cmp,
+                                       MemtableIterator* begin,
+                                       MemtableIterator* end)
+    : cmp_(cmp) {
+  table_ = new PersistentSkiplist(cmp, begin->GetNode(), end->GetNode());
 }
 
 PersistentMemtable::~PersistentMemtable() {
 }
 
-size_t PersistentMemtable::ApproximateMemoryUsage() {
-  return table_.ApproximateMemoryUsage();
-}
-
-Iterator *PersistentMemtable::NewIterator() {
-  return nullptr;
+Iterator* PersistentMemtable::NewIterator() {
+  return new MemtableIterator(table_->Head());
 }
 
 void PersistentMemtable::Add(const Slice& key, const Slice& value) {
-  table_.Insert(key, value);
+  mutex->Lock();
+  auto node = table_->Insert(key, value);
+  if (compaction_start == NULL) {
+    compaction_start = new MemtableIterator(table_, node);
+    compaction_end = new MemtableIterator(table_, node);
+    compaction_current_size += node->GetSize();
+  }
+  else if (compaction_current_size < compaction_target_size) {
+    if (cmp_->Compare(key, compaction_end->key()) > 0) {
+      compaction_end->Next();
+      compaction_current_size += compaction_end->GetSize();
+    } else {
+      compaction_current_size += node->GetSize();
+    }
+  }
+  else if (compaction_current_size >= compaction_target_size &&
+      cmp_->Compare(key, compaction_end->key()) < 0) {
+    compaction_current_size += node->GetSize() - compaction_end->GetSize();
+    compaction_end->Prev();
+  }
+  mutex->Unlock()
 }
 
 bool PersistentMemtable::Get(const Slice& key, std::string* value) {
-  auto node = table_.Find(key);
+  auto node = table_->Find(key);
   if (node != NULL) {
     value->assign(node->value);
     return true;
   }
   return false;
+}
+
+PersistentMemtable* PersistentMemtable::Compact() {
+  mutex->Lock();
+  auto node = compaction_start->GetNode()->next[0];
+  table_->Erase(compaction_start->GetNode(), compaction_end->GetNode());
+  auto imm = new PersistentMemtable(cmp_, compaction_start, compaction_end);
+  // delete iterators
+  delete compaction_start;
+  delete compaction_end;
+  // initialize new
+  compaction_start = new MemtableIterator(table_, node);
+  compaction_end = new MemtableIterator(table_, node);
+  mutex->Unlock();
+  return imm;
 }
 
 }
--- db/persistent_memtable.h
+++ db/persistent_memtable.h
@@ -9,8 +9,12 @@
 namespace leveldb {
 
 class PersistentMemtable {
+  class MemtableIterator;
  public:
-  explicit PersistentMemtable(const Comparator& cmp);
+  explicit PersistentMemtable(const Comparator* cmp);
+
+  explicit PersistentMemtable(const Comparator* cmp,
+                              MemtableIterator* begin, MemtableIterator* end);
 
   void Ref() { ++refs_; }
 
@@ -22,7 +26,9 @@ class PersistentMemtable {
     }
   }
 
-  size_t ApproximateMemoryUsage();
+  size_t ApproximateMemoryUsage() { return table_->ApproximateMemoryUsage(); }
+
+  size_t CompactionMemory() { return compaction_current_size; }
 
   Iterator* NewIterator();
 
@@ -30,12 +36,60 @@ class PersistentMemtable {
 
   bool Get(const Slice& key, std::string* value);
 
+  // Get new memtable to from current one to make compaction
+  PersistentMemtable* Compact();
+
+  class MemtableIterator : public Iterator {
+   public:
+    MemtableIterator(PersistentSkiplist* list, PersistentSkiplist::Node* node)
+        : list_(list), node_(node) { }
+
+    ~MemtableIterator() { }
+
+    bool Valid() const  { return node_ != NULL; }
+
+    void SeekToFirst() { node_ = list_->Head(); }
+
+    void SeekToLast() { node_ = list_->Tail(); }
+
+    void Seek(const Slice& target) { node_ = list_->Find(target); }
+
+    void Next() { node_ = node_->next[0]; }
+
+    void Prev() { node_ = node_->prev[0]; }
+
+    Slice key() const { return node_->key; }
+
+    Slice value() const { return node_->value; }
+
+    Status status() const { return Status::OK(); }
+
+    size_t GetSize() const { return node_->GetSize(); }
+
+    PersistentSkiplist::Node* GetNode() const { return node_; }
+
+   private:
+    PersistentSkiplist::Node* node_;
+    PersistentSkiplist* list_;
+  };
+
  private:
   ~PersistentMemtable();
 
-  PersistentSkiplist table_;
-  const Comparator cmp_;
+  PersistentSkiplist* table_;
+  const Comparator* cmp_;
   int refs_;
+
+  // access control
+  port::Mutex* mutex;
+
+  const static size_t compaction_target_size = 1 << 21; // 2mb
+
+  // compaction iterator ranges
+  MemtableIterator* compaction_start;
+  MemtableIterator* compaction_end;
+  // compaction size
+  size_t compaction_current_size;
 
   // no copy allowed
   PersistentMemtable(const PersistentMemtable&);
